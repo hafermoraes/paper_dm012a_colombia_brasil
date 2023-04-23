125867dc163d223162acfc33f99772081ac4e649
hafermoraes <hafer.moraes@gmail.com>
2023-04-23 20:21:25 -0300

    v02 dependence ratio with graphs

--- src/R/2_dependence_ratios.R
+++ src/R/2_dependence_ratios.R
@@ -42,14 +42,17 @@ raw_wide <- raw_long %>%
   )
 
 ## novamente no formato longo para basear os gráficos
-raw_wide %>%
+dr_base <- raw_wide %>%
   select( -c(`0-14`,`15-59`,`60+`)) %>% 
-  pivot_longer( cols = -(1:2)) %>% 
+  pivot_longer( cols = -(1:2)) 
+
+## comparação lado-a-lado entre países
+dr_facet_iso3code <- dr_base %>% 
   ggplot(
     aes( 
       x = year,
       y = value,
-      color = name, group = name
+      color = name
     )
   ) + 
   geom_point() + 
@@ -61,7 +64,38 @@ raw_wide %>%
     group = '',
     caption = 'United Nations, Department of Economic and Social Affairs, Population Division (2022). World Population Prospects 2022, Online Edition.'
   ) + 
+  guides(color = guide_legend(reverse=TRUE)) +
   facet_grid(~iso3_code) + 
   theme(
     legend.position = 'top'
   )
+
+dr_facet_iso3code +
+  ggsave( filename = "imgs/dependence_ratios/dr_facet_iso3code.png", width = 9, height = 5)
+
+## comparação dos países por tipo de dependência
+dr_facet_poptype <- dr_base %>% 
+  ggplot(
+    aes( 
+      x = year,
+      y = value,
+      color = iso3_code
+    )
+  ) + 
+  geom_point() + 
+  geom_line() + 
+  labs(
+    y = 'Razão de dependência',
+    x = 'ano',
+    color = '',
+    group = '',
+    caption = 'United Nations, Department of Economic and Social Affairs, Population Division (2022). World Population Prospects 2022, Online Edition.'
+  ) + 
+  guides(color = guide_legend(reverse=TRUE)) +
+  facet_wrap(~name, scales = "free_y") + 
+  theme(
+    legend.position = 'top'
+  )
+
+dr_facet_poptype +
+  ggsave( filename = "imgs/dependence_ratios/dr_facet_poptype.png", width = 9, height = 5)
