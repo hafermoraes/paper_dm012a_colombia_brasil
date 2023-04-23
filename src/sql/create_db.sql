87aef1f42bc8ba0ab495bfc541718c74476b7b2a
hafermoraes <hafer.moraes@gmail.com>
2023-04-23 17:59:37 -0300

    adapted source data to population

--- src/sql/create_db.sql
+++ src/sql/create_db.sql
@@ -6,9 +6,9 @@
 -- cria schema ~wpp2022~ para a versão 2022 dos dados da World Population Prospects
           create schema wpp2022;
 
--- Indicadores demográficos (arquivo ~WPP2022_Demographic_Indicators_Medium.csv~)
--- drop table if exists wpp2022.demographic_indicators
-           create table wpp2022.demographic_indicators (
+-- Estoque de população (arquivo ~WPP2022_PopulationBySingleAgeSex_Medium_1950-2021.csv~)
+-- drop table if exists wpp2022.population
+           create table wpp2022.population (
                         sortorder integer,
                         locid integer,
                         notes varchar(20),
@@ -22,66 +22,19 @@
                         varid integer,
                         variant varchar(255),
                         time integer,
-                        tpopulation1jan real,
-                        tpopulation1july real,
-                        tpopulationmale1july real,
-                        tpopulationfemale1july real,
-                        popdensity real,
-                        popsexratio real,
-                        medianagepop real,
-                        natchange real,
-                        natchangert real,
-                        popchange real,
-                        popgrowthrate real,
-                        doublingtime real,
-                        births real,
-                        births1519 real,
-                        cbr real,
-                        tfr real,
-                        nrr real,
-                        mac real,
-                        srb real,
-                        deaths real,
-                        deathsmale real,
-                        deathsfemale real,
-                        cdr real,
-                        lex real,
-                        lexmale real,
-                        lexfemale real,
-                        le15 real,
-                        le15male real,
-                        le15female real,
-                        le65 real,
-                        le65male real,
-                        le65female real,
-                        le80 real,
-                        le80male real,
-                        le80female real,
-                        infantdeaths real,
-                        imr real,
-                        lbsurvivingage1 real,
-                        under5deaths real,
-                        q5 real,
-                        q0040 real,
-                        q0040male real,
-                        q0040female real,
-                        q0060 real,
-                        q0060male real,
-                        q0060female real,
-                        q1550 real,
-                        q1550male real,
-                        q1550female real,
-                        q1560 real,
-                        q1560male real,
-                        q1560female real,
-                        netmigrations real,
-                        cnmr real
+                        midperiod integer,
+                        agegrp varchar(20),
+                        agegrpstart integer,
+                        agegrpspan integer,
+                        popmale real,
+                        popfemale real,
+                        poptotal real
                         );
 
--- importa indicadores demográficos na tabela ~demographic_indicators~
+-- importa população na tabela ~population~
 -- /pg_data/ existe dentro do docker do postgres!
-                   copy wpp2022.demographic_indicators
-                   from '/pg_data/WPP2022_Demographic_Indicators_Medium.csv'
+                   copy wpp2022.population
+                   from '/pg_data/WPP2022_PopulationBySingleAgeSex_Medium_1950-2021.csv'
               delimiter ','
                     csv
                  header -- ignora linha de nomes de coluna
@@ -124,8 +77,8 @@
 
 
 -- Tábuas de Vida Abreviadas (arquivo ~WPP2022_Life_Table_Abridged_Medium_1950-2021.csv~)
--- drop table if exists wpp2022.life_tables
-           create table wpp2022.life_tables (
+-- drop table if exists wpp2022.lifetables
+           create table wpp2022.lifetables (
                         sortorder integer,
                         locid integer,
                         notes varchar(20),
@@ -157,9 +110,9 @@
                         ax real
                         );
 
--- importa tábuas de vida na tabela ~life_tables~
+-- importa tábuas de vida na tabela ~lifetables~
 -- /pg_data/ existe dentro do docker do postgres!
-                   copy wpp2022.life_tables
+                   copy wpp2022.lifetables
                    from '/pg_data/WPP2022_Life_Table_Abridged_Medium_1950-2021.csv'
               delimiter ','
                     csv
