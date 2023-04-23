ffade2de7a2857a13cc309ca0843b56c35f452a2
hafermoraes <hafer.moraes@gmail.com>
2023-04-23 18:11:50 -0300

    R script updated to correct sql query

--- src/R/1_age_pyramids.R
+++ src/R/1_age_pyramids.R
@@ -5,4 +5,20 @@ library(tidyverse)
 source('R/0_connect_to_db.R')
 
 ## query the database
-dbGetQuery( conn, statement = "select * from wpp2022.life_tables limit 5;" )
+sql_query <- "
+   select iso3_code, 
+          midperiod, 
+          agegrp, 
+          popmale, 
+          popfemale, 
+          poptotal 
+     from wpp2022.population 
+    where variant = 'Medium' 
+     and iso3_code in ('COL','BRA') 
+     and midperiod in (1960,1970,1980,1990,2000,2010) 
+order by iso3_code,
+         midperiod, 
+         agegrpstart
+       ;
+"
+dbGetQuery( conn, statement = sql_query )
