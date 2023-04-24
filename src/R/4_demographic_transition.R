ecc102c885fbd44d58cb6afd58e777dd1a336601
hafermoraes <hafer.moraes@gmail.com>
2023-04-23 21:08:50 -0300

    first lines of demographic transition graph

---
+++ src/R/4_demographic_transition.R
@@ -0,0 +1,26 @@
+library(dplyr)         # mutate, %>% ,select, filter, transmute, etc...
+library(readr)         # write_csv2
+library(tidyr)         # pivot_wider, pivot_longer
+library(stringr)       # str_detect, str_sub
+library(ggplot2)       # ggplot, geom_line, geom_point, ...
+
+## to connect postgresql database (saved in 'conn' object)
+source('src/R/0_connect_to_db.R')
+
+## query the database
+sql_query <- "
+  select iso3_code,
+         time,
+         cbr,
+         cdr,
+         tpopulationmale1july + tpopulationfemale1july as pop
+    from wpp2022.demographic_indicators
+   where variant = 'Medium'
+     and iso3_code in ('COL','BRA')
+     and time in (1960,1970,1980,1990,2000,2010,2020)
+     -- and time between 1960 and 2020
+order by 1,2
+       ;
+
+"
+raw_wide <- dbGetQuery( conn, statement = sql_query )
