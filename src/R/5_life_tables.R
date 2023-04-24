d2de801c5e013b393308d4b0f8d98198a5282cf9
hafermoraes <hafer.moraes@gmail.com>
2023-04-24 19:03:50 -0300

    first lines of life table graphs

---
+++ src/R/5_life_tables.R
@@ -0,0 +1,29 @@
+library(dplyr)         # mutate, %>% ,select, filter, transmute, etc...
+library(tidyr)         # pivot_wider, pivot_longer
+library(ggplot2)       # ggplot, geom_line, geom_point, ...
+
+## to connect postgresql database (saved in 'conn' object)
+source('src/R/0_connect_to_db.R')
+
+## query the database
+sql_query <- "
+  select iso3_code,
+         time as year,
+         agegrpstart,
+         mx,
+         lx,
+         dx
+    from wpp2022.lifetables 
+   where variant = 'Medium'
+     and iso3_code in ('COL','BRA')
+     and time in (1960,1970,1980,1990,2000,2010,2020)
+order by 1,2,3
+       ;
+"
+raw_wide <- dbGetQuery( conn, statement = sql_query )
+
+raw_wide %>%
+  pivot_longer(cols = -c(1:3)) %>% View
+  ggplot( aes( x = agegrpstart, y = value, linetype = name)) + 
+  geom_line() + 
+  facet_grid(year ~ iso3_code)
