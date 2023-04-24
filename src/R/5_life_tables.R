1ab85eac76b3821e454b10ad8d1127642991a15a
hafermoraes <hafer.moraes@gmail.com>
2023-04-24 19:57:16 -0300

    v01 of life table charts

--- src/R/5_life_tables.R
+++ src/R/5_life_tables.R
@@ -9,25 +9,42 @@ source('src/R/0_connect_to_db.R')
 sql_query <- "
   select iso3_code,
          time as year,
-         agegrpstart,
+         agegrpstart as age,
+         sex,
          mx,
          lx,
          dx
-    from wpp2022.lifetables 
+    from wpp2022.lifetables_singleage
    where variant = 'Medium'
      and iso3_code in ('COL','BRA')
      and time in (1960,1970,1980,1990,2000,2010,2020)
-order by 1,2,3
+     -- and time in (1960,2020)
+order by 1,2,3,4
        ;
 "
 raw_wide <- dbGetQuery( conn, statement = sql_query )
 
-raw_wide %>% 
-  pivot_longer(cols = -c(1:3)) %>% 
-  mutate( 
-    agegrpstart = as.numeric(agegrpstart),
-    year = as.factor(year)
+## gráfico das funções da tábua de vida (inspirado por Preston 2001, p.52, figura 3.3)
+lt_plot <- raw_wide %>% 
+  transmute( 
+    iso3_code, 
+    year = as.factor(year), 
+    age = as.numeric(age),
+    log_mx = log(mx),
+    lx,
+    dx
   ) %>% 
-  ggplot( aes( x = agegrpstart, y = value, linetype = year)) + 
-  geom_line() + 
+  pivot_longer(cols = -c(1:3)) %>% 
+  ggplot( aes( x = age, y = value)) + 
+  geom_line(aes(colour = year)) + 
+  labs(
+    x = 'idade',
+    y = 'valor da função da tábua de vida',
+    color = 'ano censitário',
+    caption = 'United Nations, Department of Economic and Social Affairs, Population Division (2022). World Population Prospects 2022, Online Edition.'
+  ) + 
   facet_grid(name ~ iso3_code, scales = 'free')
+
+lt_plot + 
+  ggsave( filename = "imgs/life_tables/lt_facet_iso3code.png", width = 7, height = 10)  
+
