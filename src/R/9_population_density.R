83a976731701e11dda60462edf1e39d62e31ec5e
hafermoraes <hafer.moraes@gmail.com>
2023-05-06 17:00:39 -0300

    population density and quotient COL/BRA plots

--- src/R/9_population_density.R
+++ src/R/9_population_density.R
@@ -9,7 +9,7 @@ source('src/R/0_connect_to_db.R')
 sql_query <- "
   select iso3_code,
          time as year,
-         popdensity as pop_density,
+         popdensity as pop_density
     from wpp2022.demographic_indicators
    where variant = 'Medium'
      and iso3_code in ('COL','BRA')
@@ -17,4 +17,36 @@ sql_query <- "
 order by 1,2
     ;
 "
-raw_wide <- dbGetQuery( conn, statement = sql_query )
+raw_long <- dbGetQuery( conn, statement = sql_query )
+
+## Densidade populacional (pessoas por km quadrado)
+dens_pop_plot <- raw_long %>%
+  ggplot( aes( x = year, y = pop_density)) + 
+  geom_line(aes(color = iso3_code)) + 
+  scale_x_continuous(n.breaks = 15) + 
+  guides(color = guide_legend(reverse=TRUE)) +
+  labs(
+    x = 'ano',
+    y = 'Densidade populacional (pessoas por km²)',
+    color = 'país'
+  ) 
+
+dens_pop_plot +
+  ggsave( filename = "imgs/population_density/dens_pop.png", width = 7, height = 4)
+
+
+## Quociente COL/BRA da densidade populacional
+quoc_plot <- raw_long %>% 
+  pivot_wider(names_from = iso3_code, values_from = pop_density) %>%
+  mutate(fct_COL_BRA = COL / BRA) %>%
+  ggplot( aes( x = year, y = fct_COL_BRA)) + 
+  geom_line() + 
+  scale_x_continuous(n.breaks = 15) + 
+  labs(
+    x = 'ano',
+    subtitle = 'Quociente COL/BRA da Densidade populacional',
+    y = '[Dens.Pop.COL] / [Dens.Pop.BRA]'
+  ) 
+
+quoc_plot +
+  ggsave( filename = "imgs/population_density/quociente_denspop.png", width = 7, height = 4)
